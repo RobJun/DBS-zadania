da4bcd6cfe8d73ad9e27b69c6abbafb80965fdb3
RobJun <rob.junas@gmail.com>
2022-05-07 22:28:59 +0200

    migration

--- dbs/settings.py
+++ dbs/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
     }
 }
 
-DATABASE_ROUTERS = ['api_v4.routers.Router',]
+#DATABASE_ROUTERS = ['api_v4.routers.Router',]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
